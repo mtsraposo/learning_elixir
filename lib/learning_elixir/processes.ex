f5866e930b41987b849b0c038064a57445d5f5d7
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-24 10:52:53 -0300

    add processes

---
+++ lib/learning_elixir/processes.ex
@@ -0,0 +1,14 @@
+defmodule TakeANumber do
+  def start() do
+    spawn(fn -> loop(0) end)
+  end
+
+  def loop(state) do
+    receive do
+      {:report_state, sender_pid} -> send(sender_pid, state); loop(state)
+      {:take_a_number, sender_pid} -> send(sender_pid, state + 1); loop(state + 1)
+      :stop -> IO.puts "Stopping..."
+      _ -> loop(state)
+    end
+  end
+end
