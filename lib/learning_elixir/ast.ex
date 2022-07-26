1f304887a6a797aa4e7eeda70904427f43921295
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-26 18:45:37 -0300

    improve AST solution

--- lib/learning_elixir/ast.ex
+++ lib/learning_elixir/ast.ex
@@ -1,43 +1,30 @@
 defmodule TopSecret do
-  def to_ast(string), do: Code.string_to_quoted(string) |> elem(1)
+  def to_ast(string), do: Code.string_to_quoted!(string)
 
   def decode_secret_message_part(ast, acc) do
-    {ast, cond do
-      elem(ast, 0) in [:defp, :def] -> [decode_function_name(ast) | acc]
-      true -> acc
-    end}
+    {ast, parse(ast, acc)}
   end
 
-  defp decode_function_name({:def, _, fun} = _ast), do: decode_function_name(fun)
+  defp parse({:def, _meta, fun}, acc), do: parse(fun, acc)
+  defp parse({:defp, _meta, fun}, acc), do: parse(fun, acc)
+  defp parse([{:when, _meta, [{name, _, args}, _cond]}, _body], acc), do: [decode_function_name(name, args) | acc]
+  defp parse([{name, _, args}, _body], acc), do: [decode_function_name(name, args) | acc]
+  defp parse(_, acc), do: acc
 
-  defp decode_function_name({:defp, _, fun} = _ast), do: decode_function_name(fun)
-
-  defp decode_function_name({:when, _, [header, _]} = _guard), do: decode_function_name(header)
-
-  defp decode_function_name([header, _] = _fun), do: decode_function_name(header)
-
-  defp decode_function_name({name, _, args} = _header), do: decode_function_name(name, args)
-
+  defp decode_function_name(fun_name, args) when args == nil, do: ""
   defp decode_function_name(fun_name, args) do
+    length = max(0,Enum.count(args))
     fun_name
     |> Atom.to_string()
-    |> String.slice(0, max(0,Enum.count(args || [])))
+    |> String.slice(0, length)
   end
 
-  def decode_secret_message(string) when is_binary(string), do: decode_secret_message(to_ast(string))
-
-  def decode_secret_message({:__block__, _, body} = _ast) do
-    body
-    |> Enum.map(&decode_secret_message(&1))
+  def decode_secret_message(string) do
+    string
+    |> to_ast()
+    |> Macro.prewalk([], &decode_secret_message_part(&1, &2))
+    |> elem(1)
+    |> Enum.reverse()
     |> Enum.join()
   end
-
-  def decode_secret_message({:defmodule, _, module} = _body), do: decode_secret_message(module)
-
-  def decode_secret_message([_, [do: functions]] = _module), do: decode_secret_message(functions)
-
-  def decode_secret_message({:def, _, fun} = _function), do: decode_function_name(fun)
-
-  def decode_secret_message({:defp, _, fun} = _function), do: decode_function_name(fun)
-
 end
