95198b37b492d30b8eaf536b5b856f2243c0c863
Matheus Raposo <matheus_raposo@hotmail.com>
2022-07-26 20:50:11 -0300

    add behaviours

---
+++ lib/learning_elixir/behaviours.ex
@@ -0,0 +1,51 @@
+defmodule DancingDots.Animation do
+  @type dot :: DancingDots.Dot.t()
+  @type opts :: keyword
+  @type error :: any
+  @type frame_number :: pos_integer
+
+  # Please implement the module
+  @callback init(opts) :: {:ok, opts} | {:error, error}
+  @callback handle_frame(dot, frame_number, opts) :: dot
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour DancingDots.Animation
+      def init(opts), do: {:ok, opts}
+      defoverridable init: 1
+    end
+  end
+end
+
+defmodule DancingDots.Flicker do
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, opts) do
+    case rem(frame_number, 4) do
+      0 -> %{dot | opacity: dot.opacity / 2}
+      _ -> dot
+    end
+  end
+
+end
+
+defmodule DancingDots.Zoom do
+  use DancingDots.Animation
+
+  @impl DancingDots.Animation
+  def init(opts) do
+    velocity = Keyword.get(opts, :velocity)
+    if is_number(velocity) do
+      {:ok, opts}
+    else
+      {:error, "The :velocity option is required, and its value must be a number. Got: #{inspect(velocity)}"}
+    end
+  end
+
+  @impl DancingDots.Animation
+  def handle_frame(dot, frame_number, opts) do
+    velocity = Keyword.get(opts, :velocity)
+    %{dot | radius: dot.radius + (frame_number - 1) * velocity}
+  end
+end
