79effd7da4c69fb01b00e8874479b02183913f48
Matheus Raposo <matheus_raposo@hotmail.com>
2022-08-05 23:19:23 -0300

    add clock implementing String.Chars protocol

---
+++ lib/learning_elixir/clock.ex
@@ -0,0 +1,48 @@
+defmodule Clock do
+  defstruct hour: 0, minute: 0
+
+  @hours_in_day 24 * 60
+
+  defimpl String.Chars do
+    def to_string(%Clock{hour: hour, minute: minute}) do
+      hour = :io_lib.format("~2..*B", ["0", hour])
+      minute = :io_lib.format("~2..*B", ["0", minute])
+      "#{hour}:#{minute}"
+    end
+  end
+
+  @doc """
+  Returns a clock that can be represented as a string:
+
+      iex> Clock.new(8, 9) |> to_string
+      "08:09"
+  """
+  @spec new(integer, integer) :: Clock
+  def new(hour, minute) do
+    calc_minutes_from_midnight(hour, minute)
+    |> to_clock()
+  end
+
+  defp calc_minutes_from_midnight(hour, minute) do
+    minutes = 60 * hour + minute
+    if minutes >= 0,
+       do: rem(minutes, @hours_in_day),
+       else: @hours_in_day - rem(abs(minutes), @hours_in_day)
+  end
+
+  defp to_clock(minutes) do
+    %Clock{hour: div(minutes, 60),
+      minute: rem(minutes, 60)}
+  end
+
+  @doc """
+  Adds two clock times:
+
+      iex> Clock.new(10, 0) |> Clock.add(3) |> to_string
+      "10:03"
+  """
+  @spec add(Clock, integer) :: Clock
+  def add(%Clock{hour: hour, minute: minute}, add_minute) do
+    new(hour, minute + add_minute)
+  end
+end
